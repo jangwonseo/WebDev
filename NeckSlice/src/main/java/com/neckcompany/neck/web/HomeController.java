6337dff5843b0e47a702607b94df9fe49edda102
jangwonseo <>
2018-07-14 23:07:46 +0900

    slf4 의 구현체로 logback 사용

--- NeckSlice/src/main/java/com/neckcompany/neck/web/HomeController.java
+++ NeckSlice/src/main/java/com/neckcompany/neck/web/HomeController.java
@@ -31,7 +31,12 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date); 
