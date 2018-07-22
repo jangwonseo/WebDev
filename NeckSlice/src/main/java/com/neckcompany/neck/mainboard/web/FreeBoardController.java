5bbf2afeb9aa5adfe2fa7c2f483245ac6aa29d16
jangwonseo <>
2018-07-23 06:09:42 +0900

    minor change

--- NeckSlice/src/main/java/com/neckcompany/neck/mainboard/web/FreeBoardController.java
+++ NeckSlice/src/main/java/com/neckcompany/neck/mainboard/web/FreeBoardController.java
@@ -29,7 +29,7 @@ public class FreeBoardController {
 		});
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-		return "mainboard/mainboard";
+		return "mainboard/list";
 		
 	}
 }
